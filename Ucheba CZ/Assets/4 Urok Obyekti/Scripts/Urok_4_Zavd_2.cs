1295e5077a0b2ef5fcb7a76e63c4fbd4df669b14
TorLinn <comp6@Mac-mini-comp6.local>
2015-03-18 12:04:51 +0200

    v.150318.2
    Закінчена 2-га

--- Ucheba CZ/Assets/4 Urok Obyekti/Scripts/Urok_4_Zavd_2.cs
+++ Ucheba CZ/Assets/4 Urok Obyekti/Scripts/Urok_4_Zavd_2.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class Urok_4_Zavd_2 : MonoBehaviour {
 
 	public GameObject kolll;
+	public static GameObject [,] kollMas = new GameObject[6,6];
 
 	// Use this for initialization
 	void Start () {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				kollMas [i,j] = (GameObject)Instantiate (kolll, new Vector3 (0+i*3, 0+j*3, 0), new Quaternion (0f, 0f, 0f, 0f));
+				kollMas [i,j].transform.parent = transform;
+				kollMas [i,j].name = i+"_"+j;
+			}
+		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 	
 	}
 }
---
+++ Ucheba CZ/Assets/4 Urok Obyekti/Scripts/Urok_4_Zavd_2_Obj.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Urok_4_Zavd_2_Obj : MonoBehaviour {
+
+	int x,y;
+	string nnam;
+	public Sprite sprt;
+
+	// Use this for initialization
+	void Start () {
+
+		nnam = gameObject.name;
+		x = int.Parse (nnam.Substring (0, 1));
+		y = int.Parse (nnam.Substring (2, 1));
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter () {
+		for (int i = x-1; i < x+2; i++) {
+			for (int j = y-1; j < y+2; j++) {
+				if ((i >= 0 && i < 6) && (j >= 0 && j < 6)) {
+					Urok_4_Zavd_2.kollMas[i, j].GetComponent<SpriteRenderer> ().color = Color.green;
+				}
+			}
+		}
+		GetComponent<SpriteRenderer> ().color = Color.red;
+	}
+	
+	void OnMouseExit () {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				Urok_4_Zavd_2.kollMas[i, j].GetComponent<SpriteRenderer> ().color = Color.white;
+			}
+		}
+	}
+}
