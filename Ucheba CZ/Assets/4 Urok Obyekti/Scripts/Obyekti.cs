670b1e3d8d42336ff345d5d9bd25465a72b34b3b
Petro Ostapovets <postapovets@gmail.com>
2015-03-18 09:30:57 +0200

    += changes
    :-)

--- Ucheba CZ/Assets/4 Urok Obyekti/Scripts/Obyekti.cs
+++ Ucheba CZ/Assets/4 Urok Obyekti/Scripts/Obyekti.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Obyekti : MonoBehaviour {
+public class Obyekti : MonoBehaviour
+{
 
 	public GameObject koll;
 	public static int Count;
@@ -12,21 +13,23 @@ public class Obyekti : MonoBehaviour {
 	float timer, timer2;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 	}
 
 	// Update is called once per frame
-	void Update () {
-		time = Random.Range (2,5);
-		timer = timer + Time.deltaTime;
-		timer2 = timer2 + Time.deltaTime;
+	void Update ()
+	{
+		time = Random.Range (2, 5);
+		timer += Time.deltaTime;
+		timer2 += Time.deltaTime;
 		if (time < timer) {
 			timer = 0;
 
-			randx = Random.Range (-10,11);
-			randy = Random.Range (-3,4);
-			randz = Random.Range (0,7);
+			randx = Random.Range (-10, 11);
+			randy = Random.Range (-3, 4);
+			randz = Random.Range (0, 7);
 			obj = (GameObject)Instantiate (koll, new Vector3 (randx, randy, randz), new Quaternion (0f, 0f, 0f, 0f));
 			obj.transform.parent = transform;
 
