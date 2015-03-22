b5891f0b5f2be3002b08a164b3ca6ca6a3dc779a
TorLinn <kurach.taras@gmail.com>
2015-03-22 19:22:03 +0200

    v.150321.1
    гачало выборки

---
+++ Viborka/Assets/Scripts/ViborkaClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViborkaClick : MonoBehaviour {
+
+	//ViborkaMain cls;
+		
+	void OnMouseDown (){
+		print (gameObject.name);
+		print(transform.Find ("cilindr").transform.name);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		//cls = transform.GetComponent<ViborkaMain> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Viborka/Assets/Scripts/ViborkaMain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViborkaMain : MonoBehaviour {
+
+	public GameObject [] fig = new GameObject[7];
+	GameObject obj;
+	public int [] razm = new int[7];
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 7; i++) {
+			razm[i] = Random.Range(5,7);
+		}
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < razm[i]; j++) {
+				obj = (GameObject)Instantiate (fig[i], new Vector3 (Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0), new Quaternion (0f, 0f, 0f, 0f));
+				obj.name = fig[i].name + "_" + j;
+				obj.transform.parent = transform;
+			}
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
