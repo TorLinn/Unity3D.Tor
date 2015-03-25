e8fdf53a4704f7e46227b6cd1720a9f0b9d15577
TorLinn <comp6@Mac-mini-comp6.local>
2015-03-25 13:16:17 +0200

    v.150325.2
    Zavdannya 3

---
+++ Camera Ray/Assets/Scripts/Luch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Luch : MonoBehaviour {
+	public GameObject obj;
+	GameObject obGs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray luch = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (luch, out hit, 100f)) {
+				Vector3 objPoss = hit.point + new Vector3 (hit.normal.x*0.5f, hit.normal.y*0.5f, hit.normal.z*0.5f);
+				obGs = (GameObject) Instantiate (obj, objPoss, new Quaternion (0f, 0f, 0f, 0f));
+			}
+		}
+	
+	}
+}
