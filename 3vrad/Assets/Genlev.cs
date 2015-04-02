319d5c31ca0de99cfd8dcd681ee1d3e6b29e10f3
TorLinn <comp6@Mac-mini-comp6.local>
2015-04-02 15:22:05 +0300

    v.150402.1
    1

--- 3vrad/Assets/Genlev.cs
+++ 3vrad/Assets/Genlev.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class Genlev : MonoBehaviour {
 	public GameObject sfera;
+	GameObject selected=null;
 	public static int grany = 9;
 	public static GameObject [,] obj;
-	Color[] cVet = {Color.red, Color.gray, Color.green, Color.blue, Color.yellow};
+	Color[] cVet = {Color.red, Color.white, Color.green, Color.cyan, Color.yellow};
 
 	// Use this for initialization
 	void Start () {
@@ -23,18 +24,53 @@ public class Genlev : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		for (int i = 0; i < grany; i++) {
-			for (int j = 0; j < grany; j++) {
-				if (obj[i,j].GetComponent<MeshRenderer>().material.color != Color.clear) {
-					poisk (i,j,obj[i,j].GetComponent<MeshRenderer>().material.color);
+		if (Input.GetMouseButtonDown (0)) {
+			Ray luch = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (luch, out hit, 100f)) {
+
+				if ((selected != null) && (selected.transform.position != hit.collider.transform.position) &&(Vector3.Distance(selected.transform.position, hit.collider.transform.position)<=1)) {
+					Vector3 sectPos = selected.transform.position;
+					Vector3 hitPos = hit.collider.transform.position;
+					Vector3 [] path1 = new Vector3[3];
+					path1 [0] = new Vector3 (sectPos.x, sectPos.y, sectPos.z-1);
+					path1 [1] = new Vector3 (hitPos.x, hitPos.y, hitPos.z-1);
+					path1 [2] = new Vector3 (hitPos.x, hitPos.y, hitPos.z);
+					Vector3 [] path2 = new Vector3[3];
+					path2 [0] = new Vector3 (hitPos.x, hitPos.y, hitPos.z+1);
+					path2 [1] = new Vector3 (sectPos.x, sectPos.y, sectPos.z+1);
+					path2 [2] = new Vector3 (sectPos.x, sectPos.y, sectPos.z);
+					iTween.MoveTo (selected, iTween.Hash("path", path1, "time", 1f, "easetype", iTween.EaseType.easeInSine));
+					iTween.MoveTo (hit.collider.gameObject, iTween.Hash("path", path2, "time", 1f, "easetype", iTween.EaseType.easeInSine, "oncomplete", "DropCirles", 
+					                                                    "oncompletetarget", hit.collider.transform.parent.gameObject));
+					iTween.RotateTo (selected, iTween.Hash("z", 0));
+					for (int i = 0; i < grany; i++) {
+						for (int j = 0; j < grany; j++) {
+							if (obj [i, j] == selected)
+								obj [i, j] = hit.collider.gameObject;
+							else if (obj [i, j] == hit.collider.gameObject)
+								obj [i, j] = selected;
+						}
+					}
+					selected = null;
+					//DropCirles ();
+					return;
 				}
-					
+				else if ((selected != null) && (Vector3.Distance(selected.transform.position, hit.collider.transform.position)>1)) {
+					iTween.RotateTo (selected, iTween.Hash("z", 0));
+					selected = null;
+				}
+				if (selected == null) {
+					selected = hit.collider.gameObject;
+					selected.transform.position = hit.collider.gameObject.transform.position;
+					iTween.RotateTo (selected, iTween.Hash("z", 180, "time", 1, "easetype", iTween.EaseType.linear,"looptype", iTween.LoopType.loop));
+				}
+
 			}
 		}
-	
 	}
 
-	private void poisk (int f, int z, Color colLor) {
+	/*private void poisk (int f, int z, Color colLor) {
 		Color tempC = colLor;
 		if ((z - 1 >= 0) && (tempC == obj [f, z - 1].GetComponent<MeshRenderer>().material.color)) {
 			obj [f, z - 1].GetComponent<MeshRenderer>().material.color = Color.clear;
@@ -46,17 +82,70 @@ public class Genlev : MonoBehaviour {
 		if ((z + 1 < grany) && (tempC == obj [f, z + 1].GetComponent<MeshRenderer>().material.color)) {
 			obj [f,z+1].GetComponent<MeshRenderer>().material.color = Color.clear;
 			obj [f , z].GetComponent<MeshRenderer>().material.color = Color.clear;
+			Destroy(obj [f, z + 1]);
+			Destroy(obj [f , z]);
 			poisk (f,z+1,tempC);
 		}
 		if ((f - 1 >= 0) && (tempC == obj [f - 1, z].GetComponent<MeshRenderer>().material.color)) {
 			obj [f - 1, z].GetComponent<MeshRenderer>().material.color = Color.clear;
 			obj [f , z].GetComponent<MeshRenderer>().material.color = Color.clear;
+			Destroy(obj [f-1, z]);
+			Destroy(obj [f , z]);
 			poisk (f-1,z,tempC);
 		}
 		if ((f + 1 < grany) && (tempC == obj [f + 1, z].GetComponent<MeshRenderer>().material.color)) {
 			obj [f + 1, z].GetComponent<MeshRenderer>().material.color = Color.clear;
 			obj [f, z].GetComponent<MeshRenderer>().material.color = Color.clear;
+			Destroy(obj [f+1, z]);
+			Destroy(obj [f , z]);
 			poisk (f + 1, z, tempC);
 		}
-	}	
+	}*/
+	void DropCirles ()
+	{	
+		for (int i = 0; i < grany; i++) {
+			for (int j = 0; j < grany; j++) {
+				Color myColor = obj [i, j].GetComponent<MeshRenderer> ().material.color;
+				if (j < grany - 2) {
+					if (obj [i, j + 1].GetComponent<MeshRenderer> ().material.color == myColor && obj [i, j + 2].GetComponent<MeshRenderer> ().material.color == myColor) {
+						Vector3 pos1 = obj[i,j].transform.position;
+						Destroy(obj [i, j]);
+						obj[i,j] = (GameObject) Instantiate (sfera, pos1, new Quaternion (0,0,0,0));
+						obj[i,j].GetComponent<MeshRenderer>().material.color = cVet[Random.Range(0, 5)];
+						//iTween.MoveTo(obj[i,j], iTween.Hash("z",0, "time", 1));
+						Vector3 pos2 = obj[i,j+1].transform.position;
+						Destroy(obj [i, j+1]);
+						obj[i,j+1] = (GameObject) Instantiate (sfera, pos2, new Quaternion (0,0,0,0));
+						obj[i,j+1].GetComponent<MeshRenderer>().material.color = cVet[Random.Range(0, 5)];
+						Vector3 pos3 = obj[i,j+2].transform.position;
+						Destroy(obj [i, j+2]);
+						obj[i,j+2] = (GameObject) Instantiate (sfera, pos3, new Quaternion (0,0,0,0));
+						obj[i,j+2].GetComponent<MeshRenderer>().material.color = cVet[Random.Range(0, 5)];
+						//iTween.RotateAdd (obj [i, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+						//iTween.RotateAdd (obj [i, j + 1], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+						//iTween.RotateAdd (obj [i, j + 2], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+					}
+				}
+				if (i < grany - 2) {
+					if (obj [i + 1, j].GetComponent<MeshRenderer> ().material.color == myColor && obj [i + 2, j].GetComponent<MeshRenderer> ().material.color == myColor) {
+						Vector3 pos4 = obj[i,j].transform.position;
+						Destroy(obj [i, j]);
+						obj[i,j] = (GameObject) Instantiate (sfera, pos4, new Quaternion (0,0,0,0));
+						obj[i,j].GetComponent<MeshRenderer>().material.color = cVet[Random.Range(0, 5)];
+						Vector3 pos5 = obj[i+1,j].transform.position;
+						Destroy(obj [i+1, j]);
+						obj[i+1,j] = (GameObject) Instantiate (sfera, pos5, new Quaternion (0,0,0,0));
+						obj[i+1,j].GetComponent<MeshRenderer>().material.color = cVet[Random.Range(0, 5)];
+						Vector3 pos6 = obj[i+2,j].transform.position;
+						Destroy(obj [i+2, j]);
+						obj[i+2,j] = (GameObject) Instantiate (sfera, pos6, new Quaternion (0,0,0,0));
+						obj[i+2,j].GetComponent<MeshRenderer>().material.color = cVet[Random.Range(0, 5)];
+						//iTween.RotateAdd (obj [i, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+						//iTween.RotateAdd (obj [i + 1, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+						//iTween.RotateAdd (obj [i + 2, j], iTween.Hash ("y", 360, "time", 2, "looptype", iTween.LoopType.loop, "easetype", iTween.EaseType.linear));
+					}	
+				}
+			}
+		}
+	}
 }
--- 3vrad/Assets/Select.cs
+++ 3vrad/Assets/Select.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class Select : MonoBehaviour {
 	int x,y;
-	int por = Genlev.grany;
+	//int por = Genlev.grany;
 	string nnam;
 
 	void OnMouseDown () {
-		Color col;
-		print (gameObject.name);
 
-		if ((x-1>=0) && (Genlev.obj [x-1,y].tag == "Selected")) {
+		/*print (gameObject.name);
+
+		Color col;		if ((x-1>=0) && (Genlev.obj [x-1,y].tag == "Selected")) {
 			Genlev.obj [x-1,y].tag = "Igra";
-			//iTween.MoveAdd (Genlev.obj [x-1,y], iTween.Hash("z",-1,"x",0.5,"easetype",iTween.EaseType.linear, "time", 0.1));
-			//iTween.MoveAdd (Genlev.obj [x-1,y], iTween.Hash("z",1,"x",0.5,"easetype",iTween.EaseType.linear, "delay", 0.1, "time", 0.1));
-			//iTween.MoveAdd (Genlev.obj [x,y], iTween.Hash("z",1,"x",-0.5,"easetype",iTween.EaseType.linear, "time", 0.1));
-			//iTween.MoveAdd (Genlev.obj [x,y], iTween.Hash("z",-1,"x",-0.5,"easetype",iTween.EaseType.linear, "delay", 0.1, "time", 0.1));
+			Vector3 [] path1 = new Vector3[2];
+			path1 [0] = new Vector3 (Genlev.obj [x-1,y].transform.position.x+0.5f,
+			                         Genlev.obj [x-1,y].transform.position.y, 
+			                         Genlev.obj [x-1,y].transform.position.z-1);
+			path1 [1] = new Vector3 (Genlev.obj [x,y].transform.position.x,
+			                         Genlev.obj [x,y].transform.position.y,
+			                         Genlev.obj [x,y].transform.position.z);
+			Vector3 [] path2 = new Vector3[2];
+			path2 [1] = new Vector3 ();
+
+			//iTween.MoveAdd (Genlev.obj [x-1,y], iTween.Hash("z",-1,"x",0.5,"easetype",iTween.EaseType.linear, "time", 1));
+			//iTween.MoveAdd (Genlev.obj [x-1,y], iTween.Hash("z",1,"x",0.5,"easetype",iTween.EaseType.linear, "delay", 1, "time", 1));
+			//iTween.MoveAdd (Genlev.obj [x,y], iTween.Hash("z",1,"x",-0.5,"easetype",iTween.EaseType.linear, "time", 1));
+			//iTween.MoveAdd (Genlev.obj [x,y], iTween.Hash("z",-1,"x",-0.5,"easetype",iTween.EaseType.linear, "delay", 1, "time", 1));
+			//iTween.MoveTo (Genlev.obj [x-1,y], iTween.Hash("path", path1, "time", 0.3f, "easetype", iTween.EaseType.easeInSine));
 			col = Genlev.obj [x,y].GetComponent<MeshRenderer>().material.color;
 			Genlev.obj [x,y].GetComponent<MeshRenderer>().material.color = Genlev.obj [x-1,y].GetComponent<MeshRenderer>().material.color;
 			Genlev.obj [x-1,y].GetComponent<MeshRenderer>().material.color = col;
@@ -49,14 +60,14 @@ public class Select : MonoBehaviour {
 				Genlev.obj [i,j].tag = "Igra";
 			}
 		}
-		gameObject.tag = "Selected";	
+		gameObject.tag = "Selected";	*/
 }
 
 	// Use this for initialization
 	void Start () {
-		nnam = gameObject.name;
+		/*nnam = gameObject.name;
 		x = int.Parse (nnam.Substring (2, 2));
-		y = int.Parse (nnam.Substring (5, 2));
+		y = int.Parse (nnam.Substring (5, 2));*/
 	}
 	
 	// Update is called once per frame
