29dc7d7295d84572db614a95bc02103ac1935c73
TorLinn <comp6@Mac-mini-comp6.local>
2015-04-09 10:09:33 +0300

    v.150409.1
    FPS_Light

---
+++ Light/Assets/Scripts/TargL.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TargL : MonoBehaviour {
+
+	public GameObject targ;
+	GameObject obj;
+
+	void OnTriggerEnter (Collider col) {
+		if (col.tag == "Player") {
+			Destroy(gameObject);
+			col.transform.GetComponent<Player>().Score +=1;
+			obj = (GameObject) Instantiate (targ, new Vector3 (Random.Range (50,350), 200, Random.Range (50,350)), Quaternion.identity);
+			obj.GetComponent<Light>().enabled = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y<50) {
+			Destroy(gameObject);
+			obj = (GameObject) Instantiate (targ, new Vector3 (Random.Range (50,350), 200, Random.Range (50,350)), Quaternion.identity);
+			obj.GetComponent<Light>().enabled = true;
+		}
+	}
+}
---
+++ Light/Assets/Scripts/Targeti.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targeti : MonoBehaviour {
+	public GameObject targets;
+	GameObject obj;
+	public int Kilk = 5;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < Kilk; i++) {
+			obj = (GameObject) Instantiate (targets, new Vector3 (Random.Range (50,350), 200, Random.Range (50,350)), Quaternion.identity);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
