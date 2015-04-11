201303aebc4ad428694c258328cfd4bb79ee6384
TorLinn <kurach.taras@gmail.com>
2015-04-11 16:17:32 +0300

    v.150411.1
    katapulta nachalo

---
+++ Katapulta/Assets/Scripts/Yadro.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Yadro : MonoBehaviour {
+	Rigidbody2D person;
+
+	// Use this for initialization
+	void Start () {
+		person = GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			person.AddForce (new Vector2 (1000f, 0f));
+		}
+	}
+}
