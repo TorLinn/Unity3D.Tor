38dae004ac7f79b7eb0e5cec2599becb5b2a2545
TorLinn <kurach.taras@gmail.com>
2015-05-05 19:35:00 +0300

    v.150505.3
    dorobleno UI

---
+++ UI/Assets/Scripts/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+	public GameObject kur;
+	public GameObject icon;
+
+	public void Klik ()
+	{
+		if (kur.GetComponent<Kursor> ().kliked == false) {
+			kur.GetComponent<Kursor>().temp = gameObject;
+			kur.GetComponent<Image> ().sprite = GetComponent<Image> ().sprite;
+			kur.GetComponent<Image> ().color = new Color (1f, 1f, 1f, 1f);
+			GetComponent<Image> ().sprite = null;
+			kur.GetComponent<Kursor> ().kliked = true;
+		} else {
+			if (kur.GetComponent<Kursor>().temp != null) {
+				kur.GetComponent<Kursor>().temp.GetComponent<Image>().sprite = GetComponent<Image> ().sprite;
+				kur.GetComponent<Kursor>().temp.GetComponent<Image>().color = new Color (1f, 1f, 1f, 1f);
+			}
+			kur.GetComponent<Kursor>().temp = null;
+			GetComponent<Image> ().sprite = kur.GetComponent<Image> ().sprite;
+			GetComponent<Image> ().color = new Color (1f, 1f, 1f, 1f);
+			kur.GetComponent<Image> ().sprite = null;
+			kur.GetComponent<Kursor> ().kliked = false;
+		}
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GetComponent<Image> ().sprite == null) {
+			GetComponent<Image> ().color = new Color (1f, 1f, 1f, 0f);
+		}
+	}
+}
--- UI/Assets/Scripts/Kursor.cs
+++ UI/Assets/Scripts/Kursor.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Kursor : MonoBehaviour
 {
 
 	public bool kliked = false;
+	public GameObject temp;
 
 	// Use this for initialization
 	void Start ()
@@ -16,5 +18,8 @@ public class Kursor : MonoBehaviour
 	void Update ()
 	{
 		transform.position = Input.mousePosition + new Vector3 (-50f, 0, 0);
+		if (GetComponent<Image> ().sprite == null) {
+			GetComponent<Image> ().color = new Color (1f, 1f, 1f, 0f);
+		}
 	}
 }
