2732bc20e9f638c46e22d4eea1b9dcacdf568bbd
TorLinn <kurach.taras@gmail.com>
2015-05-05 15:14:35 +0300

    v.150505.1
    KR ta UI

---
+++ UI/Assets/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+	public GameObject kur;
+	public GameObject icon;
+	public GameObject temp;
+
+	public void Klik ()
+	{
+		if (kur.GetComponent<Kursor> ().kliked == false) {
+			temp = gameObject;
+			kur.GetComponent<Image> ().sprite = GetComponent<Image> ().sprite;
+			kur.GetComponent<Image> ().color = new Color (1f, 1f, 1f, 1f);
+			kur.GetComponent<Kursor> ().kliked = true;
+		} else {
+				
+		}
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ UI/Assets/Kursor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kursor : MonoBehaviour
+{
+
+	public bool kliked = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Input.mousePosition + new Vector3 (-50f, 0, 0);
+	}
+}
