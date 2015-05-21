1fdd0cc3f5e2acec97d6791fabcf32b786ae0720
TorLinn <kurach.taras@gmail.com>
2015-05-21 15:24:43 +0300

    v.150521.1
    multiplayer

---
+++ Multiplayer/Assets/Name_txt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Name_txt : MonoBehaviour
+{
+	public bool showBar;			//тригер для показа хитбара
+	public GameObject mon;	
+	public string nam = "Player";
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// рисование бара через GUI
+	void OnGUI ()
+	{
+		Vector3 screenPos = Camera.main.WorldToScreenPoint (mon.transform.position);
+		GUI.Label (new Rect (screenPos.x - 25, Camera.main.pixelRect.yMax - screenPos.y - 20, 100f, 20f), nam);
+	}
+}
--- Sferi/Sferi/Assets/Scripts/Ertch.cs
+++ Sferi/Sferi/Assets/Scripts/Ertch.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ertch : MonoBehaviour {
+public class Ertch : MonoBehaviour
+{
+	Vector3 aaa;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		aaa = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		transform.Rotate (0, 1f, 0); // Поворот на 1 градус
+		print (transform.position);
+		print (aaa);
 
 
 	}
